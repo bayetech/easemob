2519f4121a7723a5281230f7ffde3e6da0622e91
guochunzhong <guochunzhong@bayekeji.com>
2016-10-11 17:30:27 +0800

    Add token facility.

--- lib/easemob.rb
+++ lib/easemob.rb
@@ -1,4 +1,5 @@
 require 'easemob/version'
+require 'easemob/token'
 
 module Easemob
   class << self
@@ -8,10 +9,21 @@ module Easemob
     attr_writer :base_url
     attr_writer :org_name        # 企业的唯一标识，开发者管理后台注册账号时填写的企业 ID
     attr_writer :app_name        # 同一“企业”下“APP”唯一标识，在环信开发者管理后台创建应用时填写的“应用名称”
+
+    attr_accessor :token_file_path
   end
   @base_url = 'https://a1.easemob.com'
+  @token_file_path = '/tmp/easemob_token'
+  @random_generator = Random.new
 
   def self.head_url
     "#{@base_url}/#{@org_name}/#{@app_name}"
+  end
+
+  def self.token
+    # Possible two worker running, one worker refresh token, other unaware, so must read every time
+    access_token, remain_life_seconds = Token.read_from_store
+    Token.refresh if remain_life_seconds < @random_generator.rand(30..3 * 60)
+    access_token
   end
 end
---
+++ lib/easemob/token.rb
@@ -0,0 +1,36 @@
+require 'json'
+
+module Easemob
+  module Token
+    def self.refresh
+      data = do_post('token', JSON.generate(grant_type: 'client_credential', client_id: client_id, client_secret: client_secret))
+      write_to_store(data)
+      read_from_store
+    end
+
+    def self.read_from_store
+      td = read_token
+      token_life_in_seconds = td.fetch('token_expires_in').to_i
+      got_token_at = td.fetch('got_token_at').to_i
+      access_token = td.fetch('access_token')
+      remain_life_seconds = token_life_in_seconds - (Time.now.to_i - got_token_at)
+      return [access_token, remain_life_seconds]
+    rescue JSON::ParserError, Errno::ENOENT, KeyError, TypeError
+      refresh
+    end
+
+    def self.write_to_store(token_hash)
+      token_hash['got_token_at'.freeze] = Time.now.to_i
+      token_hash['token_expires_in'.freeze] = token_hash.delete('expires_in')
+      write_token(token_hash)
+    end
+
+    def self.read_token
+      JSON.parse(File.read(token_file_path))
+    end
+
+    def self.write_token(token_hash)
+      File.write(token_file_path, token_hash.to_json)
+    end
+  end
+end
