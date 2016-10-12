a1c2b32c007c1752ef41a2a32a6c954c6d6ebb47
guochunzhong <guochunzhong@bayekeji.com>
2016-10-12 15:08:16 +0800

    Add rspec test.

--- spec/easemob_spec.rb
+++ spec/easemob_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Easemob do
-  it 'has a version number' do
-    expect(Easemob::VERSION).not_to be nil
+  it 'can get token' do
+    expect(Easemob.token).not_to be nil
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'easemob'
+
+# It's a test account, do not using in other purpose.
+Easemob.client_id     = 'YXA6ojexMJBJEeayDK3HcysiJg'
+Easemob.client_secret = 'YXA68r4Bs1CR9aXwhWsMQ9vNsrfHKJg'
+Easemob.org_name      = 'bayetech'
+Easemob.app_name      = 'easemob'
