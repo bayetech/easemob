508b700e6e9adb503d2dce4bb6ee90b60d114895
guochunzhong <guochunzhong@bayekeji.com>
2016-10-12 21:52:55 +0800

    Also include the test account when loading console.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,7 @@ CodeClimate::TestReporter.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'easemob'
 
-# It's a test account, do not using in other purpose.
-Easemob.client_id     = 'YXA6ojexMJBJEeayDK3HcysiJg'
-Easemob.client_secret = 'YXA68r4Bs1CR9aXwhWsMQ9vNsrfHKJg'
-Easemob.org_name      = 'bayetech'
-Easemob.app_name      = 'easemob'
+load 'spec/test_account.rb'
 
 require 'rspec'
 RSpec.configure do |config|
---
+++ spec/test_account.rb
@@ -0,0 +1,5 @@
+# It's a test account, do not using in other purpose.
+Easemob.client_id     = 'YXA6ojexMJBJEeayDK3HcysiJg'
+Easemob.client_secret = 'YXA68r4Bs1CR9aXwhWsMQ9vNsrfHKJg'
+Easemob.org_name      = 'bayetech'
+Easemob.app_name      = 'easemob'
