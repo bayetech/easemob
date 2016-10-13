fb1da522d052973148ef5366dd56f5a070ddee2c
guochunzhong <guochunzhong@bayekeji.com>
2016-10-13 18:11:02 +0800

    Remove parenthesis as ruby support it.

--- lib/easemob/users.rb
+++ lib/easemob/users.rb
@@ -2,22 +2,22 @@ module Easemob
   module Users
     def create_user(username, password, nickname = nil)
       valid_username!(username)
-      request(:post, 'users', json: { username: username, password: password, nickname: nickname })
+      request :post, 'users', json: { username: username, password: password, nickname: nickname }
     end
 
     def create_users(users)
       users.map { |user| valid_username!(user['username'] || user[:username]) }
-      request(:post, 'users', json: users)
+      request :post, 'users', json: users
     end
 
     def get_user(username)
-      request(:get, "users/#{username}")
+      request :get, "users/#{username}"
     end
 
     def query_users(limit, cursor = nil)
       params = { limit: limit }
       params[:cursor] = cursor unless cursor.nil?
-      request(:get, 'user', params: params)
+      request :get, 'user', params: params
     end
 
     def delete_user(username)
