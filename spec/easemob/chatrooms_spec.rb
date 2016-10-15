78c8bd7a7d53922797d0cab59ca7afab24fe751e
guochunzhong <guochunzhong@bayekeji.com>
2016-10-15 19:42:53 +0800

    using chatroom as a word.

--- spec/easemob/chatrooms_spec.rb
+++ spec/easemob/chatrooms_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 RSpec.describe Easemob::Chatrooms do
   describe '#create_chatroom' do
-    it 'can create chat room without any member' do
-      res = Easemob.create_chatroom('c1', 'chat room 1', 'u1')
+    it 'can create chatroom without any member' do
+      res = Easemob.create_chatroom('c1', 'chatroom 1', 'u1')
       expect(res.code).to eq 200
       h1 = JSON.parse res.to_s
       expect(h1['data']['id']).not_to be nil
     end
 
-    it 'can create chat room with members' do
-      res = Easemob.create_chatroom 'c2', 'chat room 2', 'u1', members: %w(u2 u3)
+    it 'can create chatroom with members' do
+      res = Easemob.create_chatroom 'c2', 'chatroom 2', 'u1', members: %w(u2 u3)
       expect(res.code).to eq 200
       h1 = JSON.parse res.to_s
       expect(h1['data']['id']).not_to be nil
@@ -18,8 +18,8 @@ RSpec.describe Easemob::Chatrooms do
   end
 
   describe '#modify_chatroom' do
-    it 'can modify chat room with new name and description' do
-      res = Easemob.modify_chatroom($easemob_rspec_chatroom_c_id, chatroom_name: 'g', description: 'chat room after modified')
+    it 'can modify chatroom with new name and description' do
+      res = Easemob.modify_chatroom($easemob_rspec_chatroom_c_id, chatroom_name: 'g', description: 'chatroom after modified')
       expect(res.code).to eq 200
       h1 = JSON.parse res.to_s
       expect(h1['data']['name']).to be true
