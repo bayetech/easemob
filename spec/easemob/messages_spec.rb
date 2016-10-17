bd2efb75e1dca5d497b6277d3ae91f49e905ad1e
guochunzhong <guochunzhong@bayekeji.com>
2016-10-17 14:55:58 +0800

    It's very traditional hello, world
    https://en.wikipedia.org/wiki/%22Hello,_World!%22_program#History

--- spec/easemob/messages_spec.rb
+++ spec/easemob/messages_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe Easemob::Messages do
-
+  describe '#message_to' do
+    it 'can sent message to user' do
+      res = Easemob.message_to 'u1', text: "hello, world\n"
+      expect(res.code).to eq 200
+      h1 = JSON.parse res.to_s
+      expect(h1['data']).not_to be nil
+      expect(h1['data']['u1']).to eq 'success'
+    end
+  end
 end
