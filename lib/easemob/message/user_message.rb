7d9e70249c9032f8af2c10729ef511aa4d110361
houdelin <houdelin@bayekeji.com>
2016-12-01 20:10:50 +0800

    add user message

---
+++ lib/easemob/message/user_message.rb
@@ -0,0 +1,6 @@
+require_relative 'base_message'
+
+module Easemob
+  class UserMessage < Easemob::BaseMessage
+  end
+end
--- lib/easemob/users.rb
+++ lib/easemob/users.rb
@@ -1,89 +1,90 @@
 module Easemob
+  autoload(:UserMessage, File.expand_path('message/user_message', __dir__))
   module Users
     def create_user(username, password, nickname: nil)
       valid_username!(username)
       jd = { username: username, password: password }
       jd[:nickname] = nickname unless nickname.nil?
-      request :post, 'users', json: jd
+      UserMessage.new request :post, 'users', json: jd
     end
 
     def create_users(users)
       users.map { |user| valid_username!(user['username'] || user[:username]) }
-      request :post, 'users', json: users
+      UserMessage.new request :post, 'users', json: users
     end
 
     def get_user(username)
-      request :get, "users/#{username}"
+      UserMessage.new request :get, "users/#{username}"
     end
 
     def query_users(limit = 50, cursor: nil)
       params = { limit: limit }
       params[:cursor] = cursor unless cursor.nil?
-      request :get, 'users', params: params
+      UserMessage.new request :get, 'users', params: params
     end
 
     def delete_user(username)
-      request :delete, "users/#{username}"
+      UserMessage.new request :delete, "users/#{username}"
     end
 
     def delete_users!(number = 100)
-      request :delete, 'users', params: { limit: number }
+      UserMessage.new request :delete, 'users', params: { limit: number }
     end
 
     def reset_user_password(username, newpassword:)
-      request :put, "users/#{username}/password", json: { newpassword: newpassword }
+      UserMessage.new request :put, "users/#{username}/password", json: { newpassword: newpassword }
     end
 
     def set_user_nickname(username, nickname:)
-      request :put, "users/#{username}", json: { nickname: nickname }
+      UserMessage.new request :put, "users/#{username}", json: { nickname: nickname }
     end
 
     def add_user_friend(owner_username, friend_username:)
-      request :post, "users/#{owner_username}/contacts/users/#{friend_username}"
+      UserMessage.new request :post, "users/#{owner_username}/contacts/users/#{friend_username}"
     end
 
     def remove_user_friend(owner_username, friend_username:)
-      request :delete, "users/#{owner_username}/contacts/users/#{friend_username}"
+      UserMessage.new request :delete, "users/#{owner_username}/contacts/users/#{friend_username}"
     end
 
     def query_user_friends(owner_username)
-      request :get, "users/#{owner_username}/contacts/users"
+      UserMessage.new request :get, "users/#{owner_username}/contacts/users"
     end
 
     def add_to_user_block(owner_username, to_block_usernames:)
-      request :post, "users/#{owner_username}/blocks/users", json: { usernames: [*to_block_usernames] }
+      UserMessage.new request :post, "users/#{owner_username}/blocks/users", json: { usernames: [*to_block_usernames] }
     end
 
     def remove_from_user_block(owner_username, blocked_username:)
-      request :delete, "users/#{owner_username}/blocks/users/#{blocked_username}"
+      UserMessage.new request :delete, "users/#{owner_username}/blocks/users/#{blocked_username}"
     end
 
     def query_user_blocks(owner_username)
-      request :get, "users/#{owner_username}/blocks/users"
+      UserMessage.new request :get, "users/#{owner_username}/blocks/users"
     end
 
     def get_user_status(username)
-      request :get, "users/#{username}/status"
+      UserMessage.new request :get, "users/#{username}/status"
     end
 
     def get_user_offline_msg_count(owner_username)
-      request :get, "users/#{owner_username}/offline_msg_count"
+      UserMessage.new request :get, "users/#{owner_username}/offline_msg_count"
     end
 
     def get_user_offline_msg_status(username, msg_id)
-      request :get, "users/#{username}/offline_msg_status/#{msg_id}"
+      UserMessage.new request :get, "users/#{username}/offline_msg_status/#{msg_id}"
     end
 
     def deactivate_user(username)
-      request :post, "users/#{username}/deactivate"
+      UserMessage.new request :post, "users/#{username}/deactivate"
     end
 
     def activate_user(username)
-      request :post, "users/#{username}/activate"
+      UserMessage.new request :post, "users/#{username}/activate"
     end
 
     def disconnect_user(username)
-      request :get, "users/#{username}/disconnect"
+      UserMessage.new request :get, "users/#{username}/disconnect"
     end
 
     private
