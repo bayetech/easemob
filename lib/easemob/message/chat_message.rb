d706a31344f47464e4144a656fca132860d30826
houdelin <houdelin@bayekeji.com>
2016-12-02 16:29:27 +0800

    add all success for chat message

--- lib/easemob/message/chat_message.rb
+++ lib/easemob/message/chat_message.rb
@@ -2,5 +2,12 @@ require_relative 'base_message'
 
 module Easemob
   class ChatMessage < Easemob::BaseMessage
+
+    def all_success?
+      flag = true
+      @data.values.each{ |ar| flag = false if ar != 'success' }
+
+      flag
+    end
   end
 end
