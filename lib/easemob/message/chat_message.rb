53f4e2f0e3552b98870d3831f53ecf33c291b37b
houdelin <houdelin@bayekeji.com>
2016-12-05 15:19:19 +0800

    change chat message all_success

--- lib/easemob/message/chat_message.rb
+++ lib/easemob/message/chat_message.rb
@@ -4,10 +4,7 @@ module Easemob
   class ChatMessage < Easemob::BaseMessage
 
     def all_success?
-      flag = true
-      @data.values.each{ |ar| flag = false and return if ar != 'success' }
-
-      flag
+      @data.values.all? { |ar| ar == 'success' }
     end
   end
 end
