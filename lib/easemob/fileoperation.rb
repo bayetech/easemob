0472ece8176c3a85f100ca978654c1b7650eec82
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-08 17:27:34 +0800

    style improvement, no logic change

--- lib/easemob/fileoperation.rb
+++ lib/easemob/fileoperation.rb
@@ -3,8 +3,8 @@ module Easemob
   module Fileoperation
     def upload_chatfile(file_path, restrict_access: true)
       FileoperationMessage.new request :upload, 'chatfiles', form: { file: HTTP::FormData::File.new(file_path),
-                                            hack: 'X' }, # Existing here for http-form_data 1.0.1 handle single param improperly, see https://github.com/httprb/form_data.rb/issues/4
-                                       restrict_access: restrict_access
+                                                                     hack: 'X' }, # Existing here for http-form_data 1.0.1 handle single param improperly, see https://github.com/httprb/form_data.rb/issues/4
+                                                             restrict_access: restrict_access
     end
 
     def download_chatfile(chatfile_uuid, share_secret: nil, thumbnail: false)
